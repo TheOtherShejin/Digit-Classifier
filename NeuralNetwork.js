e9cac714ac371b4f35f403e72b172f91cfddaec1
Shejin01 <5sfamilykolliyath@gmail.com>
2024-11-20 18:26:40 +0530

    Use Softmax Neural Network

--- NeuralNetwork.js
+++ NeuralNetwork.js
@@ -1,7 +1,8 @@
 class Layer {
-    constructor(numOfNodes, numOfIncomingNodes) {
+    constructor(numOfNodes, numOfIncomingNodes, activationFunction) {
         this.numOfNodes = numOfNodes;
         this.numOfIncomingNodes = numOfIncomingNodes;
+        this.activationFunction = activationFunction;
 
         this.weights = Array.from(Array(numOfNodes), () => Array(numOfIncomingNodes).fill(0));
         this.biases = Array(numOfNodes).fill(0);
@@ -14,8 +15,9 @@ class Layer {
             for (let j = 0; j < this.numOfIncomingNodes; j++) {
                 output[i] += this.weights[i][j] * input[j];
             }
-            output[i] = Sigmoid(output[i]);
+            if (this.activationFunction === "Sigmoid") output[i] = Sigmoid(output[i]);
         }
+        if (this.activationFunction === "SoftMax") output = SoftMax(output);
         return output;
     }
 
@@ -28,12 +30,28 @@ class Layer {
     }
 }
 
+function Sigmoid(input) {
+    return 1 / (1 + Math.exp(-input));
+}
+
+function SoftMax(vector) {
+    let output = Array(vector.length).fill(0);
+    let sum = 0;
+    for (let i = 0; i < vector.length; i++) {
+        output[i] = Math.exp(vector[i]);
+        sum += output[i];
+    }
+    output.forEach((num, idx, arr) => { arr[idx] /= sum });
+    return output;
+}
+
 class NeuralNetwork {
-    constructor(layersSize) {
+    constructor(layersSize, hiddenAF, outputAF) {
         this.layers = Array(layersSize.length - 1);
         for (let i = 0; i < layersSize.length-1; i++) {
-            this.layers[i] = new Layer(layersSize[i+1], layersSize[i]);
+            this.layers[i] = new Layer(layersSize[i+1], layersSize[i], hiddenAF);
         }
+        this.layers[layersSize.length-2].activationFunction = outputAF;
     }
 
     Evaluate(input) {
@@ -57,10 +75,6 @@ class NeuralNetwork {
     }
 }
 
-function Sigmoid(input) {
-    return 1 / (1 + Math.exp(-input));
-}
-
 function MatrixToVector(mat) {
     let vector = Array(mat.length * mat[0].length);
     for (let i = 0; i < mat.length; i++) {
@@ -69,17 +83,6 @@ function MatrixToVector(mat) {
         }
     }
     return vector;
-}
-
-function SoftMax(vector) {
-    let output = Array(vector.length).fill(0);
-    let sum = 0;
-    for (let i = 0; i < vector.length; i++) {
-        output[i] = Math.exp(vector[i]);
-        sum += output[i];
-    }
-    output.forEach((num, idx, arr) => { arr[idx] /= sum });
-    return output;
 }
 
 function MaxVectorIndex(vector) {
@@ -99,5 +102,5 @@ function Predict(input) {
 }
 
 function PredictConfidence(input) {
-    return SoftMax(nn.Evaluate(MatrixToVector(input)))
+    return nn.Evaluate(MatrixToVector(input))
 }
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
-var graph = document.getElementById("graph");
-var gctx = graph.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+const graph = document.getElementById("graph");
+const gctx = graph.getContext('2d');
+const body = document.getElementById("body");
 
-var pixelWidth = canvas.width / 28;
-var pixelHeight = canvas.height / 28;
-var canvasX = canvas.getBoundingClientRect().x;
-var canvasY = canvas.getBoundingClientRect().y;
+let pixelWidth = canvas.width / 28;
+let canvasX = canvas.getBoundingClientRect().x;
+let canvasWidth = canvas.getBoundingClientRect().width;
 
-var pixels = Array.from(Array(28), () => Array(28).fill(0));
+let pixels = Array.from(Array(28), () => Array(28).fill(0));
 
-var nn = new NeuralNetwork(model["config"]);
-nn.SetAllWeights(model["weights"]);
-nn.SetAllBiases(model["biases"]);
+const nn = new NeuralNetwork(model.config, "Sigmoid", "SoftMax");
+nn.SetAllWeights(model.weights);
+nn.SetAllBiases(model.biases);
 
 function DrawPixel(x, y, brightness) {
     ctx.beginPath();
     ctx.fillStyle = `rgb(${brightness}, ${brightness}, ${brightness})`;
-    ctx.fillRect(x * pixelWidth, y * pixelHeight, pixelWidth, pixelHeight);
+    ctx.fillRect(x * pixelWidth, y * pixelWidth, pixelWidth, pixelWidth);
     ctx.stroke();
 }
 
@@ -30,11 +30,12 @@ function ClearPixelData() {
 function Clear() {
     ClearCanvas();
     ClearPixelData();
+    DrawGraph(PredictConfidence(pixels));
 }
 
 function DrawBlurredCanvas() {
     ClearCanvas();
-    let blurredPixels = BlurCanvas();
+    const blurredPixels = BlurCanvas();
     for (let i = 0; i < 28; i++) {
         for (let j = 0; j < 28; j++) {
             DrawPixel(i, j, blurredPixels[j][i]);
@@ -43,7 +44,7 @@ function DrawBlurredCanvas() {
 }
 
 function BlurCanvas() {
-    let blurredPixels = Array.from(Array(28), () => Array(28).fill(0));
+    const blurredPixels = Array.from(Array(28), () => Array(28).fill(0));
     const scalar = 1/2;
     const kernel = [
         [1/4, 1/2, 1/4],
@@ -67,53 +68,69 @@ function BlurCanvas() {
 }
 
 function DrawGraph(input) {
-    let prediction = MaxVectorIndex(input);
+    const prediction = MaxVectorIndex(input);
     gctx.clearRect(0, 0, graph.clientWidth, graph.clientHeight);
     gctx.beginPath();
-    gctx.font = "24px Arial";
+    const fontSize = 24 * graph.clientHeight / 504;
+    gctx.font = `${fontSize}px Arial`;
     for (let i = 0; i < input.length; i++) {
         gctx.fillStyle = "rgb(53, 83, 253)";
         gctx.fillRect(0, i*(graph.height / 10), input[i]*graph.width, graph.height / 10);
-        if (i == prediction) gctx.fillStyle = "white";
+        if (i === prediction) gctx.fillStyle = "white";
         else gctx.fillStyle = "rgb(150, 150, 150)";
-        gctx.fillText(i, 10, (i+1)*(graph.height/10)-12);
-        gctx.fillText(`${Math.round(input[i] * 10000) / 100}%`, graph.width - 90, (i+1)*(graph.height/10)-12);
+        
+        gctx.textAlign = "left";
+        gctx.fillText(i, 10, (i+1)*(graph.height/10)-fontSize*0.5);
+        gctx.textAlign = "right";
+        gctx.fillText(`${Math.round(input[i] * 10000) / 100}%`, graph.width - 10, (i+1)*(graph.height/10)-fontSize*0.5);
     }
     gctx.stroke();
 }
 
 function Update(x, y) {
-    if (x >= 0 && y >= 0 && x < 28 && y < 28) {
-        pixels[y][x] = 255;
-        DrawBlurredCanvas();
-        DrawGraph(PredictConfidence(BlurCanvas()));
-    }
+    if (x < 0 || y < 0 || x >= 28 || y >= 28) return;
+    pixels[y][x] = 255;
+    DrawBlurredCanvas();
+    DrawGraph(PredictConfidence(BlurCanvas()));
 }
 
-window.onmousemove = function(e) {
+window.onmousemove = (e) => {
     if (e.buttons & 1) {
-        let x = Math.floor((e.clientX - canvasX) / pixelWidth);
-        let y = Math.floor((e.clientY + scrollY - canvasY) / pixelHeight);
+        const x = Math.floor((e.clientX - canvasX) / pixelWidth);
+        const y = Math.floor((e.clientY - canvas.getBoundingClientRect().y) / pixelWidth);
         Update(x, y); 
     }
 }
-window.onload = function() {
-    if (IsPortrait()) graph.width = 504;
-    else graph.width = 252;
-    DrawGraph(PredictConfidence(BlurCanvas()));
+window.onload = () => {
+    if (IsPortrait()) alert("Portrait mode detected. This website is best viewed in landscape mode.");
+    Resize();
 }
-window.onkeydown = function(e) {
-    if (e.key == 'r') {
+window.onkeydown = (e) => {
+    if (e.key === 'r') {
         Clear();
         DrawGraph(PredictConfidence(BlurCanvas()));
     }
 }
-window.onresize = function(e) {
-    canvasX = canvas.getBoundingClientRect().x;
-    canvasY = canvas.getBoundingClientRect().y;
+window.onresize = (e) => {
+    Resize();
+}
 
-    if (IsPortrait()) graph.width = 504;
-    else graph.width = 252;
+function Resize() {
+    canvasX = canvas.getBoundingClientRect().x;
+
+    if (IsPortrait()) {
+        graph.width = graph.clientWidth;
+        graph.height = graph.clientHeight;
+    }
+    else {
+        graph.width = 252;
+        graph.height = 504;
+    }
+
+    canvasWidth = canvas.clientWidth;
+    canvas.width = canvasWidth;
+    canvas.height = canvasWidth;
+    pixelWidth = Math.round(canvasWidth / 28);
 
     DrawGraph(PredictConfidence(BlurCanvas()));
 }
@@ -123,14 +140,28 @@ function IsTouchEnabled() {
            ( navigator.maxTouchPoints > 0 ) ||
            ( navigator.msMaxTouchPoints > 0 );
 }
+
 if (IsTouchEnabled()) {
     document.addEventListener("touchmove", (e) => {
-        let x = Math.floor((e.touches[0].clientX - canvasX) / pixelWidth);
-        let y = Math.floor((e.touches[0].clientY + window.scrollY - canvasY) / pixelHeight);
-        Update(x, y);
+        const x = Math.floor((e.touches[0].clientX - canvasX) / pixelWidth);
+        const y = Math.floor((e.touches[0].clientY - canvas.getBoundingClientRect().y) / pixelWidth);
+        if (x >= 0 && x < 28 && y >= 0 && y < 28) {
+            Update(x, y);
+        }
     });
-}
 
+    document.addEventListener("touchstart", (e) => {
+        const x = Math.floor((e.touches[0].clientX - canvasX) / pixelWidth);
+        const y = Math.floor((e.touches[0].clientY - canvas.getBoundingClientRect().y) / pixelWidth);
+        if (x >= 0 && x < 28 && y >= 0 && y < 28) {
+            body.style.overflowY = "clip";
+        }
+    })
+
+    document.addEventListener("touchend", (e) => {
+        body.style.overflowY = "scroll";
+    })
+}
 function IsPortrait() {
     return document.body.clientWidth < 800;
 }
